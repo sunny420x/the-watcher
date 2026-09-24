6923fbf8575a3501ccc804598f509740b71c49ca
Sunny420x <costerraid@gmail.c.com>
2026-09-25 00:08:30 +0700

    feat: enable trust proxy

--- run.js
+++ run.js
@@ -24,10 +24,9 @@ const db = mysql.createPool({
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({ extended: true }))
-
 app.use(express.json());
-
 app.use(express.static(path.join(__dirname, 'public')));
+app.enable('trust proxy'); 
 
 const error_page = ['404 Not Found', 'Not Found', 'Unauthorized', '403 Forbidden', 'Access forbidden!', '500 - Internal server error.', 'Service Unavailable', '403 - Forbidden: Access is denied.']
 const router_page = ['Login', 'RouterOS', 'F612C', '&#70;&#54;&#56;&#56;']
